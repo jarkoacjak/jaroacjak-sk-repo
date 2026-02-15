b3d2d6e1cb629e3d912aeba843016f9cf5c780de
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-02-15 15:37:30 +0100

    Update radio list with additional radio station details

--- main.py
+++ main.py
@@ -5,34 +5,45 @@ import xbmcplugin
 def main():
     handle = int(sys.argv[1])
     
-    # Zoznam rádií (tu si môžeš pridávať ďalšie)
+    # Zoznam rádií s URL adresami a LOGAMI
     radia = [
+        {
+            "nazov": "Rádio Expres",
+            "url": "https://stream.expres.sk/128.mp3",
+            "logo": "https://www.expres.sk/wp-content/themes/expres2017/img/logo-expres.png",
+            "zaner": "Pop"
+        },
         {
             "nazov": "Fun Rádio",
             "url": "https://stream.funradio.sk:8000/fun128.mp3",
+            "logo": "https://www.funradio.sk/static/images/logo.png",
             "zaner": "Pop / Dance"
         },
         {
             "nazov": "Rádio Vlna",
             "url": "https://stream.radiovlna.sk/vlna-128.mp3",
+            "logo": "https://www.radiovlna.sk/static/images/logo.png",
             "zaner": "Oldies"
         }
     ]
 
     for radio in radia:
-        # Vytvoríme položku so správnym názvom
         list_item = xbmcgui.ListItem(label=radio["nazov"])
         
-        # Nastavíme informácie, aby Kodi vedelo, čo zobrazuje
+        # Nastavenie loga (Art)
+        list_item.setArt({
+            'thumb': radio["logo"],
+            'icon': radio["logo"],
+            'fanart': radio["logo"]
+        })
+        
+        # Nastavenie informácií
         list_item.setInfo('video', {
             'title': radio["nazov"],
             'genre': radio["zaner"]
         })
         
-        # Označíme, že ide o prehrateľný súbor (audio stream)
         list_item.setProperty('IsPlayable', 'true')
-        
-        # Pridáme do zoznamu
         xbmcplugin.addDirectoryItem(handle, radio["url"], list_item, False)
 
     xbmcplugin.endOfDirectory(handle)
