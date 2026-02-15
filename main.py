7e70af8c6e42ad92009b1eec47a2db980a95bd01
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-02-15 16:14:14 +0100

    Refactor radio listings and update logos
    Updated radio list for Slovakia and Czech Republic with new logos and URLs.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import urllib.parse
 import xbmcgui
 import xbmcplugin
 
-# Funkcia na vytváranie odkazov v menu
 def build_url(query):
     return sys.argv[0] + '?' + urllib.parse.urlencode(query)
 
@@ -11,15 +10,13 @@ def main():
     handle = int(sys.argv[1])
     params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
 
-    # --- 1. HLAVNÉ MENU (Výber krajiny) ---
+    # --- 1. HLAVNÉ MENU ---
     if not params:
-        # Priečinok Slovensko
         url_sk = build_url({'country': 'sk'})
         li_sk = xbmcgui.ListItem(label="[B]🇸🇰 SLOVENSKÉ RÁDIÁ[/B]")
         li_sk.setArt({'icon': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Flag_of_Slovakia.svg/200px-Flag_of_Slovakia.svg.png'})
         xbmcplugin.addDirectoryItem(handle, url_sk, li_sk, True)
 
-        # Priečinok Česko
         url_cz = build_url({'country': 'cz'})
         li_cz = xbmcgui.ListItem(label="[B]🇨🇿 ČESKÉ RÁDIÁ[/B]")
         li_cz.setArt({'icon': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_Czech_Republic.svg/200px-Flag_of_the_Czech_Republic.svg.png'})
@@ -27,15 +24,39 @@ def main():
 
         xbmcplugin.endOfDirectory(handle)
 
-    # --- 2. ZOZNAM SLOVENSKÝCH RÁDIÍ (Pridané Rádio Melody) ---
+    # --- 2. ZOZNAM SLOVENSKÝCH RÁDIÍ (OPRAVENÉ LOGÁ) ---
     elif params.get('country') == 'sk':
         radia_sk = [
-            {"nazov": "Rádio Slovensko", "url": "https://icecast.stv.livebox.sk/slovensko_128.mp3", "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/R%C3%A1dio_Slovensko.svg/1200px-R%C3%A1dio_Slovensko.svg.png"},
-            {"nazov": "Rádio Melody", "url": "https://stream.radiomelody.sk/melody-128.mp3", "logo": "https://www.radiomelody.sk/static/images/logo.png"},
-            {"nazov": "Rádio Beta", "url": "http://109.71.67.102:8000/beta_live_high.mp3", "logo": "https://www.betaradio.sk/wp-content/themes/beta-radio/img/logo.png"},
-            {"nazov": "Rádio Expres", "url": "https://stream.expres.sk/128.mp3", "logo": "https://www.expres.sk/wp-content/themes/expres2017/img/logo-expres.png"},
-            {"nazov": "Fun Rádio", "url": "https://stream.funradio.sk:8000/fun128.mp3", "logo": "https://www.funradio.sk/static/images/logo.png"},
-            {"nazov": "Rádio Vlna", "url": "https://stream.radiovlna.sk/vlna-128.mp3", "logo": "https://www.radiovlna.sk/static/images/logo.png"}
+            {
+                "nazov": "Rádio Slovensko", 
+                "url": "https://icecast.stv.livebox.sk/slovensko_128.mp3", 
+                "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/R%C3%A1dio_Slovensko.svg/1200px-R%C3%A1dio_Slovensko.svg.png"
+            },
+            {
+                "nazov": "Rádio Expres", 
+                "url": "https://stream.expres.sk/128.mp3", 
+                "logo": "https://www.radia.sk/_radia/loga/coverflow/expres.png"
+            },
+            {
+                "nazov": "Rádio Melody", 
+                "url": "https://stream.bauermedia.sk/melody-hi.mp3", 
+                "logo": "https://www.radiomelody.sk/cover.png?f=raw"
+            },
+            {
+                "nazov": "Rádio Beta", 
+                "url": "http://109.71.67.102:8000/beta_live_high.mp3", 
+                "logo": "https://myonlineradio.sk/public/uploads/radio_img/radio-beta/play_250_250.webp"
+            },
+            {
+                "nazov": "Fun Rádio", 
+                "url": "https://stream.funradio.sk:8000/fun128.mp3", 
+                "logo": "https://yt3.googleusercontent.com/ytc/AIdro_niUH455B3J3TNWuMR1jez81UxI_mXNeMfNZ0RyU7nWWA=s900-c-k-c0x00ffffff-no-rj"
+            },
+            {
+                "nazov": "Rádio Vlna", 
+                "url": "https://stream.radiovlna.sk/vlna-128.mp3", 
+                "logo": "https://play-lh.googleusercontent.com/1XTpQPfIg8cZLeASZoAEJlHivVgu5mOlclO8JqFn2hMoY2LlXHdsmkKjeWX6L_9t_Q"
+            }
         ]
         zobraz_radia(handle, radia_sk)
 
@@ -50,14 +71,13 @@ def main():
         ]
         zobraz_radia(handle, radia_cz)
 
-# Spoločná funkcia na zobrazenie rádií v zozname
 def zobraz_radia(handle, zoznam):
     for radio in zoznam:
         li = xbmcgui.ListItem(label=radio["nazov"])
         li.setArt({
             'thumb': radio["logo"],
             'icon': radio["logo"],
-            'fanart': radio["logo"]
+            'poster': radio["logo"]
         })
         li.setInfo('video', {'title': radio["nazov"]})
         li.setProperty('IsPlayable', 'true')
