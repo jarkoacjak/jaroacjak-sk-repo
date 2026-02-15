580d7f3d4012eb19ece64bda445087e9cb409cf7
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-02-15 15:31:55 +0100

    Refactor radio streaming to use a list of stations

--- main.py
+++ main.py
@@ -4,12 +4,39 @@ import xbmcplugin
 
 def main():
     handle = int(sys.argv[1])
-    # Tu je tvoje rádio
-    url = "https://stream.funradio.sk:8000/fun128.mp3"
-    list_item = xbmcgui.ListItem(label="Zapnúť Moje Rádio")
-    xbmcplugin.addDirectoryItem(handle, url, list_item, False)
+    
+    # Zoznam rádií (tu si môžeš pridávať ďalšie)
+    radia = [
+        {
+            "nazov": "Fun Rádio",
+            "url": "https://stream.funradio.sk:8000/fun128.mp3",
+            "zaner": "Pop / Dance"
+        },
+        {
+            "nazov": "Rádio Vlna",
+            "url": "https://stream.radiovlna.sk/vlna-128.mp3",
+            "zaner": "Oldies"
+        }
+    ]
+
+    for radio in radia:
+        # Vytvoríme položku so správnym názvom
+        list_item = xbmcgui.ListItem(label=radio["nazov"])
+        
+        # Nastavíme informácie, aby Kodi vedelo, čo zobrazuje
+        list_item.setInfo('video', {
+            'title': radio["nazov"],
+            'genre': radio["zaner"]
+        })
+        
+        # Označíme, že ide o prehrateľný súbor (audio stream)
+        list_item.setProperty('IsPlayable', 'true')
+        
+        # Pridáme do zoznamu
+        xbmcplugin.addDirectoryItem(handle, radio["url"], list_item, False)
+
     xbmcplugin.endOfDirectory(handle)
 
 if __name__ == '__main__':
     main()
-  
+    
