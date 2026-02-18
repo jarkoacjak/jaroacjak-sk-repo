963eb454120d072c37a0788849a067c5c19717de
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-02-18 16:39:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def main():
             {"nazov": "Rádio Viva", "url": "http://stream.sepia.sk:8000/viva320.mp3", "logo": "https://myonlineradio.sk/public/uploads/radio_img/radio-viva/play_250_250.webp"},
             {"nazov": "Fresh Rádio", "url": "https://icecast2.radionet.sk/freshradio.sk", "logo": "https://myonlineradio.sk/public/uploads/radio_img/fresh-radio/play_250_250.webp"},
             {"nazov": "Rádio Rock", "url": "https://stream.bauermedia.sk/rock-hi.mp3", "logo": "https://radiorock.sk/intro-v2.png"},
+            {"nazov": "HITRÁDIO SLOVAKIA", "url": "https://hitradioslovakia.stream.laut.fm/hitradioslovakia", "logo": "https://myonlineradio.sk/public/uploads/radio_img/hitradio-slovakia/play_250_250.webp"},
             {"nazov": "G - Rádio", "url": "http://88.212.34.18:8000/aacphighband.aac", "logo": "https://www.radia.sk/_radia/loga/coverflow/g-radio.png"},
             {"nazov": "Fun Rádio Mileniálky", "url": "https://stream.funradio.sk:8000/milenialky128.mp3", "logo": "https://www.radia.sk/_radia/loga/coverflow/fun-milenialky.png"},
             {"nazov": "Fun Rádio Leto", "url": "https://stream.funradio.sk:8000/summer128.mp3", "logo": "https://www.radia.sk/_radia/loga/coverflow/fun-letne-hity.png"},
@@ -100,4 +101,4 @@ def zobraz_radia(handle, zoznam):
 
 if __name__ == '__main__':
     main()
-
+        
