234409ab6327616c9b2e18e6d4e43401ce612fff
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-03-08 12:51:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,49 +42,22 @@ def main():
 
     # --- 3. ZOZNAM SLOVENSKÝCH TV ---
     elif params.get('mode') == 'list_live_sk':
-        # Overené hlavičky pre stabilitu JOJ streamov
+        # Overené hlavičky pre stabilitu streamov
         headers = "|User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36&Referer=https://videoportal.joj.sk/"
         
         tv_stanice = [
+            {"nazov": "TV JOJ", "url": "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8" + headers, "logo": "https://yt3.googleusercontent.com/8rPXBoj2l1nhd9C-DCXF-s3tx0i_36GJzJcxeMyYvyPpPNakQsyc5DYc5d_QLDeI74ILkmFSJQ=s900-c-k-c0x00ffffff-no-rj"},
+            {"nazov": "JOJ Plus", "url": "https://live.cdn.joj.sk/live/andromeda/plus-1080.m3u8" + headers, "logo": "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=100063553059682"},
+            {"nazov": "JOJ Krimi", "url": "https://live.cdn.joj.sk/live/andromeda/wau-1080.m3u8" + headers, "logo": "https://www.interez.sk/wp-content/uploads/2026/02/krimi-joj-wau-televizia.jpg"},
+            {"nazov": "JOJ Šport", "url": "https://live.cdn.joj.sk/live/andromeda/joj_sport-1080.m3u8" + headers, "logo": "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=100072427963738"},
+            {"nazov": "JOJ 24", "url": "https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8" + headers, "logo": "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=100086546375437"},
+            {"nazov": "Jojko", "url": "https://live.cdn.joj.sk/live/andromeda/jojko-1080.m3u8" + headers, "logo": "https://yt3.googleusercontent.com/ytc/AIdro_k5JuOcgs57bLqO5jeAGqVWKskOxWRaYh1_mD7PYcE4Wg=s900-c-k-c0x00ffffff-no-rj"},
+            {"nazov": "JOJ Cinema", "url": "https://live.cdn.joj.sk/live/andromeda/cinema-1080.m3u8" + headers, "logo": "http://googleusercontent.com/profile/picture/2"},
+            {"nazov": "JOJ Family", "url": "https://live.cdn.joj.sk/live/andromeda/family-1080.m3u8" + headers, "logo": "https://staticeu.sweet.tv/images/cache/channel_icons/BDUQMIAK/873-joj-family-hd.png"},
             {
-                "nazov": "TV JOJ", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8" + headers, 
-                "logo": "https://yt3.googleusercontent.com/8rPXBoj2l1nhd9C-DCXF-s3tx0i_36GJzJcxeMyYvyPpPNakQsyc5DYc5d_QLDeI74ILkmFSJQ=s900-c-k-c0x00ffffff-no-rj"
-            },
-            {
-                "nazov": "JOJ Plus", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/plus-1080.m3u8" + headers, 
-                "logo": "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=100063553059682"
-            },
-            {
-                "nazov": "JOJ Krimi", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/wau-1080.m3u8" + headers, 
-                "logo": "https://www.interez.sk/wp-content/uploads/2026/02/krimi-joj-wau-televizia.jpg"
-            },
-            {
-                "nazov": "JOJ Šport", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/joj_sport-1080.m3u8" + headers, 
-                "logo": "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=100072427963738"
-            },
-            {
-                "nazov": "JOJ 24", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8" + headers, 
-                "logo": "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=100086546375437"
-            },
-            {
-                "nazov": "Jojko", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/jojko-1080.m3u8" + headers, 
-                "logo": "https://yt3.googleusercontent.com/ytc/AIdro_k5JuOcgs57bLqO5jeAGqVWKskOxWRaYh1_mD7PYcE4Wg=s900-c-k-c0x00ffffff-no-rj"
-            },
-            {
-                "nazov": "JOJ Cinema", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/cinema-1080.m3u8" + headers, 
-                "logo": "http://googleusercontent.com/profile/picture/2"
-            },
-            {
-                "nazov": "JOJ Family", 
-                "url": "https://live.cdn.joj.sk/live/andromeda/family-1080.m3u8" + headers, 
-                "logo": "https://staticeu.sweet.tv/images/cache/channel_icons/BDUQMIAK/873-joj-family-hd.png"
+                "nazov": "CS History", 
+                "url": "https://live.cdn.joj.sk/live/andromeda/cs_history-1080.m3u8" + headers, 
+                "logo": "https://img.joj.sk/418430b1-b598-40d1-8552-39b473c73836"
             }
         ]
         
