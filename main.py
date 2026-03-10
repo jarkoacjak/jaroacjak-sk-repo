333a7766bef4b6374dc26950993f5dd1fc2e4e43
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-03-10 16:27:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,74 +2,84 @@ import sys
 import urllib.parse
 import xbmcgui
 import xbmcplugin
-import xbmcaddon
+import xbmc
 
-# Načítanie doplnku a jeho nastavení
-addon = xbmcaddon.Addon()
-
+# Funkcia na vytvorenie URL odkazov v menu
 def build_url(query):
     return sys.argv[0] + '?' + urllib.parse.urlencode(query)
 
+# Funkcia, ktorá vypýta meno a heslo hneď po kliknutí na doplnok
+def prihlasenie():
+    dialog = xbmcgui.Dialog()
+    
+    # Okno pre používateľské meno
+    user = dialog.input('Prihlásenie - Zadajte meno:', type=xbmcgui.INPUT_ALPHANUM)
+    if not user: return None, None
+    
+    # Okno pre heslo (text bude skrytý hviezdičkami)
+    password = dialog.input('Prihlásenie - Zadajte heslo:', type=xbmcgui.INPUT_ALPHANUM, isPassword=True)
+    if not password: return None, None
+    
+    return user, password
+
 def main():
-    # 1. NAČÍTANIE ÚDAJOV Z NASTAVENÍ (vytvoril si v resources/settings.xml)
-    user_name = addon.getSetting('email')
-    user_pass = addon.getSetting('password')
-
-    # 2. KONTROLA ADMINA
-    if user_name == "Admin" and user_pass == "Kanianka8":
-        # Ak je meno a heslo správne, otvoríme menu
-        zobraz_menu()
-    else:
-        # Ak sú údaje prázdne alebo zlé, ukážeme chybu
-        xbmcgui.Dialog().ok('Flow play', 'Prístup zamietnutý!\nZadajte správne údaje v nastaveniach.\n\nMeno: Admin\nHeslo: Kanianka8')
-        # Otvorí nastavenia automaticky, aby to mal používateľ ľahšie
-        addon.openSettings()
-        return
-
-def zobraz_menu():
     handle = int(sys.argv[1])
     arg_string = sys.argv[2][1:] if len(sys.argv[2]) > 1 else ""
     params = dict(urllib.parse.parse_qsl(arg_string))
 
-    # --- HLAVNÁ OBRAZOVKA DOPLNKU ---
+    # Ak používateľ práve otvoril doplnok (sme v hlavnom menu)
     if not params:
-        kategorie = [
-            {"label": "Relácie", "mode": "relacie"},
-            {"label": "Logistika", "mode": "logistika"},
-            {"label": "Filmy", "mode": "filmy"}
-        ]
+        user, password = prihlasenie()
 
-        for kat in kategorie:
-            # Vytvorenie položky v zozname
-            li = xbmcgui.ListItem(label="[B]" + kat["label"] + "[/B]")
-            url = build_url({'mode': kat['mode']})
-            # True znamená, že ide o priečinok (otvorí ďalšiu úroveň)
-            xbmcplugin.addDirectoryItem(handle, url, li, True)
+        # KONTROLA: Meno musí byť Admin a heslo Kanianka8
+        if user == "Admin" and password == "Kanianka8":
+            xbmcgui.Dialog().notification('Flow play', 'Vitaj, Admin!', xbmcgui.NOTIFICATION_INFO, 3000)
+            zobraz_hlavne_menu(handle)
+        else:
+            # Ak sú údaje nesprávne, vypíšeme chybu a skončíme
+            xbmcgui.Dialog().ok('Flow play: Prístup odmietnutý', 'Zadané údaje nie sú správne.\nSkúste to znova.')
+            return
+    else:
+        # Ak už sme prihlásení a klikáme na kategórie (Relácie, Filmy...)
+        spracuj_kategoriu(handle, params)
+
+def zobraz_hlavne_menu(handle):
+    # Zoznam tvojich kategórií
+    kategorie = [
+        {"label": "Relácie", "mode": "relacie"},
+        {"label": "Logistika", "mode": "logistika"},
+        {"label": "Filmy", "mode": "filmy"}
+    ]
+
+    for kat in kategorie:
+        # [B] urobí text tučným
+        li = xbmcgui.ListItem(label="[B]" + kat["label"] + "[/B]")
+        url = build_url({'mode': kat['mode']})
+        # True znamená, že položka sa správa ako priečinok
+        xbmcplugin.addDirectoryItem(handle, url, li, True)
+    
+    xbmcplugin.endOfDirectory(handle)
+
+def spracuj_kategoriu(handle, params):
+    mode = params.get('mode')
+    
+    # Text, ktorý sa zobrazí v každej sekcii
+    oznam = "Už čoskoro pripravujeme..."
+    
+    if mode == 'relacie':
+        li = xbmcgui.ListItem(label="[I]Sekcia Relácie - " + oznam + "[/I]")
+        xbmcplugin.addDirectoryItem(handle, "", li, False)
         
-        xbmcplugin.endOfDirectory(handle)
-
-    # --- SEKCIA RELÁCIE ---
-    elif params.get('mode') == 'relacie':
-        xbmcgui.Dialog().notification('Flow play', 'Už čoskoro pripravujeme...', xbmcgui.NOTIFICATION_INFO, 4000)
-        li = xbmcgui.ListItem(label="[I]Relácie - Už čoskoro pripravujeme[/I]")
+    elif mode == 'logistika':
+        li = xbmcgui.ListItem(label="[I]Sekcia Logistika - " + oznam + "[/I]")
         xbmcplugin.addDirectoryItem(handle, "", li, False)
-        xbmcplugin.endOfDirectory(handle)
-
-    # --- SEKCIA LOGISTIKA ---
-    elif params.get('mode') == 'logistika':
-        xbmcgui.Dialog().notification('Flow play', 'Už čoskoro pripravujeme...', xbmcgui.NOTIFICATION_INFO, 4000)
-        li = xbmcgui.ListItem(label="[I]Logistika - Už čoskoro pripravujeme[/I]")
+        
+    elif mode == 'filmy':
+        li = xbmcgui.ListItem(label="[I]Sekcia Filmy - " + oznam + "[/I]")
         xbmcplugin.addDirectoryItem(handle, "", li, False)
-        xbmcplugin.endOfDirectory(handle)
 
-    # --- SEKCIA FILMY ---
-    elif params.get('mode') == 'filmy':
-        xbmcgui.Dialog().notification('Flow play', 'Filmy pripravujeme...', xbmcgui.NOTIFICATION_INFO, 4000)
-        li = xbmcgui.ListItem(label="[I]Filmy pripravujeme...[/I]")
-        xbmcplugin.addDirectoryItem(handle, "", li, False)
-        xbmcplugin.endOfDirectory(handle)
+    xbmcplugin.endOfDirectory(handle)
 
 if __name__ == '__main__':
     main()
-
-
+    
